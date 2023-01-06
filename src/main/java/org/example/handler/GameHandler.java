4053698920477e91ed6751f5f81b3b98a2d92672
zengdm <7833340+zendm@user.noreply.gitee.com>
2023-01-06 18:25:26 +0800

    被缴械的随从无法攻击

--- src/main/java/org/example/handler/GameHandler.java
+++ src/main/java/org/example/handler/GameHandler.java
@@ -264,6 +264,10 @@ public class GameHandler {
             info.msgToThisPlayer("无法让护符卡攻击:"+amuletCard.getId());
             return;
         } else if (myCard instanceof FollowCard followCard) {
+            if(followCard.hasKeyword("缴械")){
+                info.msgToThisPlayer("被缴械的随从无法攻击！");
+                return;
+            }
             if(followCard.getTurnAge() == 0 &&
                 !followCard.hasKeyword("突进") &&
                 !followCard.hasKeyword("疾驰")){
